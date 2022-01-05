7b63c10c84b287a86173e1db1cc787e57e6c084c
invader237 <67508521+invader237@users.noreply.github.com>
2022-01-05 22:47:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,1 @@
-import function as f
-from function import MOVEMENTS, board
 
-for coords in f.checkPossibleMoves(1):
-    for direction in MOVEMENTS:
-        print(direction)
-#        if f.countSurroundingCell(1, (coords[0],coords[1]), direction) > 0:
-        
