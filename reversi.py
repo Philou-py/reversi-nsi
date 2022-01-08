1dfdbe65e8e7d6ab61e7f67e6190a67786e3a8a8
invader237 <67508521+invader237@users.noreply.github.com>
2022-01-08 09:46:16 +0100

    :memo:

--- reversi.py
+++ reversi.py
@@ -1,16 +1,31 @@
 import function
+import loadingscreen
+import shutil, os
+from time import sleep
+from os import system
+import os
 
+#displays the loading screen
+loadingscreen.launch()
+
+#
+system("mode con:cols=150 lines=80")
+
+#set the current player to one
 player = 1
+
 gameOver = False
 
 while not gameOver:
+    #call the finctioon to clear the screen
     function.clearScreen()
+
     possibleMoves = function.checkPossibleMoves(player)
+    #count the number of cell to display it after
     cellNumb = function.cellCount()
 
     if cellNumb["total"] == 64:
-        # All squares are filled
-        # Mark the game as over
+        # Mark the game as over if all the cell are filed
         game_over = True
 
         # Handle the end of the game to print out the winner and statistics
@@ -23,3 +38,5 @@ while not gameOver:
         coords = function.renderer(player, possibleMoves, cellNumb)
         function.updateBoard(coords, player)
         player = function.getOtherPlayer(player)
+
+system("mode con:cols=150 lines=80")
